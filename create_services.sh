cef6a8bb6d219d67b07587400e58d4b1a00edd65
jaimegag <jaguilar@pivotal.io>
2015-10-09 10:24:57 -0400

    Create services script

---
+++ create_services.sh
@@ -0,0 +1,4 @@
+cf cs p-config-server standard config-service
+cf cs p-service-registry standard service-registry
+cf cs p-circuit-breaker-dashboard standard circuit-breaker
+cf cs p-mysql 100mb gbfortune-db
