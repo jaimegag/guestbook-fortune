1ee51c42a8684ff9c7ae73cb0d15fda4681d9dc9
Jaime Gonzalez Aguilar <jaguilar@pivotal.io>
2016-02-24 16:44:45 -0500

    Update create_services.sh

--- create_services.sh
+++ create_services.sh
@@ -1,4 +1,4 @@
 cf cs p-config-server standard config-server
 cf cs p-service-registry standard service-registry
 cf cs p-circuit-breaker-dashboard standard circuit-breaker
-cf cs p-mysql 100mb gbfortune-db
+cf cs p-mysql 100mb-dev gbfortune-db
