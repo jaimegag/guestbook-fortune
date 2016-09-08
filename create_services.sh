73736f108f741267dfba15abe720c6ffc71bc705
jaimegag <jaguilar@pivotal.io>
2016-09-07 23:02:24 -0400

    config-server repo added in manifest

--- create_services.sh
+++ create_services.sh
@@ -1,4 +1,4 @@
-cf cs p-config-server standard config-server
+cf cs p-config-server standard config-server -c '{"git":{"uri":"https://github.com/jaimegag/config-repo"}}'
 cf cs p-service-registry standard service-registry
 cf cs p-circuit-breaker-dashboard standard circuit-breaker
 cf cs p-mysql 100mb-dev gbfortune-db
